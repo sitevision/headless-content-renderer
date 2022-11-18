b3069a863441941ce05b0d294d9d89b6cbef84f7
Albin Öhrn <albin.ohrn@sitevision.se>
2022-11-18 16:48:33 +0100

    Fixed actions
    Squashed commit of the following:
    
    commit 53623a738c3ff37c051219231b70cbaa522a9a2f
    Author: Albin Öhrn <albin.ohrn@sitevision.se>
    Date:   Fri Nov 18 16:47:59 2022 +0100
    
        No screenshot or video
    
    commit a037658ec26a84d905b10f4a65b1bae575a7d481
    Author: Albin Öhrn <albin.ohrn@sitevision.se>
    Date:   Fri Nov 18 16:44:35 2022 +0100
    
        Correct (?) indentation
    
    commit 8bfd907833ac924b613b68a2530b2142bfca02be
    Author: Albin Öhrn <albin.ohrn@sitevision.se>
    Date:   Fri Nov 18 16:40:54 2022 +0100
    
        TEst
    
    commit a1a4a52d906c08d04e1b7cbc4d1f7e1330a3d5b0
    Author: Albin Öhrn <albin.ohrn@sitevision.se>
    Date:   Fri Nov 18 16:39:23 2022 +0100
    
        Limited action

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
+    screenshotOnRunFailure: false,
+    video: false,
     specPattern: '**/*.test.tsx',
     devServer: {
       framework: 'react',
